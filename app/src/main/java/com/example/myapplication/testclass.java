d0b7604910c7ccabcd4ff730cdd01d05795c5139
cho1music <laclean@hotmail.com>
2019-08-14 13:36:05 +0900

    test

--- app/src/main/java/com/example/myapplication/testclass.java
+++ app/src/main/java/com/example/myapplication/testclass.java
@@ -2,7 +2,7 @@ package com.example.myapplication;
 
 public class testclass {
     static String str = "Kang seungjin";
-    static String str2 = "---!!!!!!!!!";
+    static String str2 = "cho";
     // 이것은 Test branch 입니다
     // 작업자 : Seungjin Kang
 }
